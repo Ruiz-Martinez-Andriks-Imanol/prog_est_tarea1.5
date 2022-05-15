b106691376d1419939a6a27d6655d9b0dfd91b18
Ruiz-Martinez-Andriks-Imanol <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-15 14:21:14 -0500

    prog_est_tarea1.5
    Programa para calcular el área de un trapecio.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # prog_est_tarea1.5
-Programa para calcular el área del trapecio
+<p>Autor: Andriks Imanol Ruiz Mártinez, Fecha: 03/02/2022 <br>
+Escuela: Universidad del Valle de Mexico Campus Villahermosa <br>
+Materia: Programación Estructrada <br>
+Ciclo: 01/2022</p>
+<br>
+<p>Este es un programa en Lenguaje C de la materia de Programación Estructurada<br>
+Muestra el uso de:
+  <ul>
+    <li>Variables enteras y flotantes</li>
+    <li>printf para mostrar mensajes y variables</li>
+    <li>scanf</li>
+    <li>El uso de include para las librerías</li>
+    <li>Condicionar a flotante</li>
+    <li>Comentarios para la documentación interna del programa</li>
+    </ul>
+    </p>
+<br>
+Objetivo: Hacer un programa que calcule el área de un trapecio.
+<br>
+<br>
+<p>Breve descripción:
+Es un programa para calcular el área del trapecio, condicionando los valores de las variables como flotante.
+<br>
+</p>
---
+++ tarea1.5.cpp
@@ -0,0 +1,34 @@
+/* Autor: Andriks Imanol Ruiz Martínez, Realizado: 03/02/2022
+Escuela: Universidad del Valle de Mexico Campus Villahermosa
+Materia: Programación Estructrada
+Ciclo: 01/2022
+
+Este es un programa en Lenguaje C de la materia de Programación Estructurada
+Muestra el uso de:
+
+    -Variables enteras y flotantes
+    -printf para mostrar mensajes y variables
+    -scanf
+    -El uso de include para las librerías
+    -Condicionar a flotante
+    -Comentarios para la documentación interna del programa
+*/	
+#include <stdio.h>
+int main() {
+	//Declaración de variable
+	int B, b, h;
+	float area;
+	//Entradas de Datos
+	printf("Calcular el área del Trapecio \n");
+	printf("Introduzca B:");
+	scanf("%d", &B);
+	printf("Introduzca b:");
+	scanf("%d", &b);
+	printf("Introduzca h:");
+	scanf("%d", &h);
+	//Proceso
+	area=(float)(B+b)*h/2;
+	//Salida
+	printf("El area del trapecio es %f", area);
+	return 0;
+}	
